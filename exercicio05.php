91279f6089fc278d38401625589fadfc88f9b644
Fernanda788 <fernanda_anna@estudante.sesisenai.org.br>
2025-08-29 12:18:08 -0300

    Exercício 05 - Feito em sala

--- exercicio05.php
+++ exercicio05.php
@@ -9,28 +9,46 @@
 
 <body>
     <form method="POST" action="">
-        <label for="numero">Digite um número:</label>
-        <input type="number" id="numero" name="numero" required>
-        <button type="submit" name="verificar_divisor">Verificar</button>
+        <label for="numero1">Digite um número:</label>
+        <input type="number1" id="numero1" name="numero1" required>
+        <br><br>
+        <label for="numero2">Digite outro número:</label>
+        <input type="number2" id="numero2" name="numero2" required>
+        <br><br>
+        <button type="submit" name="verificar_somas">Verificar</button>
     </form>
 
     <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['verificar_divisor'])) {
-        $NUMERO = $_POST['numero'];
-        $divisores = [];
-        for ($i = 1; $i <= $NUMERO; $i++) {
-            if ($NUMERO % $i == 0) { 
-                echo $i . "<br>";
-                $divisores[] = $i;
-            }
+        if (isset($_POST['verificar_somas'])) { // Se há um conteúdo e o botão é pressionado...
+            $NUMERO1 = $_POST['numero1'];
+            $NUMERO2 = $_POST['numero2'];
+
+            function somaDivisores($numero)  {  
+                $soma = 0;  
+                for ($i = 1; $i <= $numero; $i++) {  
+                    if ($numero % $i == 0) {  
+                        $soma += $i;  
+                    }  
+                }  
+                return $soma;  
+                }
+            } 
+                $somaDivisores1 = somaDivisores($NUMERO1);
+                $somaDivisores2 = somaDivisores($NUMERO2);
+
+                if($somaDivisores1 == $NUMERO2 && $somaDivisores2 == $NUMERO1) {
+                    echo "$NUMERO1 e $NUMERO2 são números amigos :)";
+                } else {
+                    echo "$NUMERO1 e $NUMERO2 não são números amigos :(";
+                }
         }
-        $soma1 = array_sum($divisores);
-        echo "E a soma dos divisores é: $soma1";
-    }
-}
 
     ?>
+
+
 </body>
+
+</html>
 
 </html>
